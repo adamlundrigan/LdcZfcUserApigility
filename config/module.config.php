02bcc2d484113ae5284c61d93ee541a226894698
Anton <brotherosirus@gmail.com>
2017-06-01 17:34:59 +0300

    create db adapter factory to inject service manager

--- config/module.config.php
+++ config/module.config.php
@@ -7,9 +7,9 @@ return array(
     ),
     'service_manager' => array(
         'invokables' => array(
-            'ldc-zfc-user-oauth2-authentication-adapter-db' => 'LdcZfcUserOAuth2\Authentication\Adapter\Db',
         ),
         'factories' => array(
+            'ldc-zfc-user-oauth2-authentication-adapter-db' => 'LdcZfcUserOAuth2\Authentication\Adapter\DbFactory',
             'ldc-zfc-user-oauth2-storage-pdo' => 'LdcZfcUserOAuth2\Storage\ZfcUserPdoFactory',
             'ldc-zfc-user-oauth2-storage-bridge' => 'LdcZfcUserOAuth2\Storage\ZfcUserStorageBridgeFactory',
         ),
